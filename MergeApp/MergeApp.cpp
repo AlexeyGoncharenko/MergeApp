2028b2995ccc9628d2149267290c203f8b63ecb7
AlexeyGoncharenko <alexey.goncharenko85@gmail.com>
2019-04-16 10:39:00 +0300

    Fixed bug: Vector is out of range. This release rely on vector

--- MergeApp/MergeApp.cpp
+++ MergeApp/MergeApp.cpp
@@ -6,11 +6,13 @@
 ///prototypes
 ///</summary>
 std::vector<int> sort(std::vector<int>, size_t, size_t);
-std::vector<int> merge(std::vector<int>, size_t, size_t, size_t);
+void merge(std::vector<int>&, size_t, size_t, size_t);
 
 int main() {
 	std::vector<int> unsortedData{5, 2, 6, 4, 1, 3, 2, 6};
 	std::vector<int> sortedData = sort(unsortedData, 0, unsortedData.size() - 1);
+	for (const auto &a : unsortedData) std::cout << a << ", "; std::cout << std::endl;
+	for (const auto &b : sortedData) std::cout << b << ", ";
 	return EXIT_SUCCESS;
 }
 
@@ -27,17 +29,20 @@ std::vector<int> sort(std::vector<int> data, size_t lowerBound, size_t upperBoun
 				std::swap(data[i], data[i + 1]);
 		}			
 	}
+	return data;
 }
 
-std::vector<int> merge(std::vector<int> data, size_t lowerBound, size_t halfdBound, size_t upperBound) {
-	size_t lSize = halfdBound - lowerBound;
-	size_t rSize = upperBound - halfdBound;
+void merge(std::vector<int> &data, size_t lowerBound, size_t halfdBound, size_t upperBound) {
+	size_t lSize = (halfdBound - lowerBound) + 1;
+	size_t rSize = (upperBound - halfdBound);
 	int *lBuffer = new int[lSize];
 	int *rBuffer = new int[rSize];
-	std::copy(data.at(lowerBound), data.at(halfdBound), lBuffer);
-	std::copy(data.at(halfdBound + 1), data.at(upperBound), rBuffer);
+	
+	std::copy(&data[lowerBound], &data[halfdBound], lBuffer);
+	std::copy(&data[halfdBound + 1], &data[upperBound], rBuffer);
+	
 	size_t i, j, k;
-	for (i = 0, j = 0, k = 0; i <= lSize && j <= rSize; k++) {
+	for (i = 0, j = 0, k = 0; i < lSize && j < rSize; k++) {
 		if (lBuffer[i] < rBuffer[j]) {
 			data[k] = lBuffer[i];
 			i++;
@@ -47,16 +52,19 @@ std::vector<int> merge(std::vector<int> data, size_t lowerBound, size_t halfdBou
 			j++;
 		}
 	}
-	while (i <= lSize) {
+
+	while (i < lSize) {
 		data[k] = lBuffer[i];
 		i++;
 		k++;
 	}
-	while (j <= rSize) {
+
+	while (j < rSize) {
 		data[k] = rBuffer[j];
 		j++;
 		k++;
 	}
+
 	delete[] lBuffer;
 	delete[] rBuffer;
 }
