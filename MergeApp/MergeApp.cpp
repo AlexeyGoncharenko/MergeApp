eaf5fb11a61e05463e0f277a887e6e5b1f0e2695
AlexeyGoncharenko <alexey.goncharenko85@gmail.com>
2019-04-16 19:35:25 +0300

    Final version merge function. With use external copy, and internal reference to resolve sorting. Return point to arranged(sorted data).

--- MergeApp/MergeApp.cpp
+++ MergeApp/MergeApp.cpp
@@ -2,49 +2,53 @@
 #include <iostream>
 #include <vector>
 
-///<summary>
-///prototypes
-///</summary>
 std::vector<int> sortArray(const std::vector<int>&, size_t, size_t);
 void sort(std::vector<int>&, size_t, size_t);
 void merge(std::vector<int>&, size_t, size_t, size_t);
 
 int main() {
-	std::vector<int> unsortedData{5, 2, 6, 4, 1, 3, 2, 6};
+	std::vector<int> unsortedData{9, 2, 6, 4, 1, 3, 2, 0};
 	std::vector<int> sortedData = sortArray(unsortedData, 0, unsortedData.size() - 1);
-	for (const auto &a : unsortedData) std::cout << a << ", "; std::cout << std::endl;
-	for (const auto &b : sortedData) std::cout << b << ", ";
+	for (const auto &a : unsortedData)	std::cout << a << ", ";	std::cout << std::endl;
+	for (const auto &b : sortedData)	std::cout << b << ", ";	std::cout << std::endl;
 	return EXIT_SUCCESS;
 }
 
 std::vector<int> sortArray(const std::vector<int> &data, size_t lowerBound, size_t upperBound) {
-	if (!data.data()){
+	if (!data.data()) {
 		return {};
 	}
 	else {
 		std::vector<int> copyData(data);
-		if (lowerBound < upperBound - 1) {
-			size_t halfBound = (size_t)std::trunc((lowerBound + upperBound) / 2);
-			sort(data, lowerBound, halfBound);
-			sort(data, halfBound + 1, upperBound);
-			merge(data, lowerBound, halfBound, upperBound);
+		sort(copyData, lowerBound, upperBound);
+		return copyData;
+	}
+}
+
+// Первичная декомпозиция рекурсивным методом блока данных до размера 2,
+// последующая сортировка смежных элементов каждой из отсортированных частей [lowerBound, halfBound], [halfBound + 1, upperBound]
+// с передачей управления функции merge для окончательной сортировки на каждом этапе декомпозиции входного блока данных.
+void sort(std::vector<int>& data, size_t lowerBound, size_t upperBound) {
+	if (lowerBound < upperBound - 1) {
+		size_t halfBound = (size_t)std::trunc((lowerBound + upperBound) / 2);
+		sort(data, lowerBound, halfBound);
+		sort(data, halfBound + 1, upperBound);
+		merge(data, lowerBound, halfBound, upperBound);
+	}
+	else {
+		for (size_t i = lowerBound; i < upperBound; i++) {
+			if (data[i] > data[i + 1])
+				std::swap(data[i], data[i + 1]);
 		}
-		else {
-			for (size_t i = lowerBound; i < upperBound; i++) {
-				if (data[i] > data[i + 1]) 
-					std::swap(data[i], data[i + 1]);
-			}			
-		}
-		return data;
 	}
 }
 
 void merge(std::vector<int> &data, size_t lowerBound, size_t halfdBound, size_t upperBound) {
+	// Выделение дополнительного объема памяти для выполнения сортировки.
 	size_t lSize = (halfdBound - lowerBound) + 1;
 	size_t rSize = (upperBound - halfdBound);
 	std::vector<int> lBuffer(lSize);
-	std::vector<int> rBuffer(rSize);
-	
+	std::vector<int> rBuffer(rSize);	
 	std::copy(data.begin() + lowerBound, data.begin() + (halfdBound + 1), lBuffer.begin());
 	std::copy(data.begin() + (halfdBound + 1), data.begin() + (upperBound + 1), rBuffer.begin());
 	
